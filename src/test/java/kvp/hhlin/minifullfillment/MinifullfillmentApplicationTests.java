068a69e98d795fb58e4d7c59eb0de0cbb1d0b1f1
hyelin <sjgpfls@gmail.com>
2022-07-10 22:29:12 +0900

    🎉 MINI Fullfillment Service 프로젝트 시작

---
+++ src/test/java/kvp/hhlin/minifullfillment/MinifullfillmentApplicationTests.java
@@ -0,0 +1,13 @@
+package kvp.hhlin.minifullfillment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinifullfillmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
