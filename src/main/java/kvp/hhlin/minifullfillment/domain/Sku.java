3445598713298eb1b69dc352d68105cf4e278446
hyelin <sjgpfls@gmail.com>
2022-07-18 01:18:28 +0900

    🔨 Sku status 타입변경, setter 제외, 메소드 순서 변경, barcodes로 네이밍 변경, 생성시점 validation check

--- src/main/java/kvp/hhlin/minifullfillment/domain/Sku.java
+++ src/main/java/kvp/hhlin/minifullfillment/domain/Sku.java
@@ -11,70 +11,52 @@ public class Sku {
 
     private String name;
 
-    private String status;
+    private SkuStatus status;
 
-    private Set<Barcode> barcode;
+    private Set<Barcode> barcodes;
 
     Sku(String code, String name) {
+        isValidCode(code);
+        isValidName(name);
         this.code = code;
         this.name = name;
-        this.status = SkuStatus.valueOf("READY").getStatus();
-        this.barcode = new HashSet<>();
+        this.status = SkuStatus.READY;
+        this.barcodes = new HashSet<>();
+    }
+
+    public void isValidCode(String code) {
+        if (code.isBlank()) {
+            throw new IllegalArgumentException("코드는 빈 값일 수 없습니다.");
+        }
+    }
+
+    public void isValidName(String name) {
+        if (!isValidLength(name) || name.isBlank()) {
+            throw new IllegalArgumentException("이름은 빈 값일 수 없고 최대 20자입니다.");
+        }
     }
 
     private boolean isValidLength(String name) {
         return name.length() <= MAX_NAME_LENGTH;
     }
 
-    private boolean isEmpty(String value) {
-        return value.trim().length() == 0 || value.isEmpty() || value.isBlank();
-    }
-
-    public void isValidName(String name) {
-        if (!isValidLength(name) || isEmpty(name)) {
-            throw new IllegalArgumentException("이름은 빈 값일 수 없고 최대 20자입니다.");
-        }
-    }
-
-    public void isValidCode(String code) {
-        if (isEmpty(code)) {
-            throw new IllegalArgumentException("코드는 빈 값일 수 없습니다.");
-        }
-    }
-
-    public void addBarcode(String barcode) {
-        this.barcode.add(Barcode.of(barcode));
+    public void addBarcode(Barcode barcode) {
+        this.barcodes.add(barcode);
     }
 
     public String getCode() {
         return code;
     }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getStatus() {
+    public SkuStatus getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
     public Set<Barcode> getBarcode() {
-        return barcode;
-    }
-
-    public void setBarcode(Set<Barcode> barcode) {
-        this.barcode = barcode;
+        return barcodes;
     }
 }
--- src/test/java/kvp/hhlin/minifullfillment/domain/SkuTest.java
+++ src/test/java/kvp/hhlin/minifullfillment/domain/SkuTest.java
@@ -17,45 +17,40 @@ public class SkuTest {
 
     @Test
     void Sku는_빈값이_아닌_코드값을_가진다() {
-        sku.setCode("");
-        assertThrows(IllegalArgumentException.class, () -> {
-            sku.isValidCode(sku.getCode());
-        });
-
-        sku.setCode("1234");
         assertNotNull(sku.getCode());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            sku = new Sku("", "초콜릿");
+        });
     }
 
     @Test
     void Sku는_빈값이_아닌_이름을_가진다() {
-        sku.setName("");
-        assertThrows(IllegalArgumentException.class, () -> {
-            sku.isValidName(sku.getName());
-        });
-
-        sku.setName("1234");
         assertNotNull(sku.getName());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            sku = new Sku("s123", "");
+        });
     }
 
     @Test
     void 이름은_최대_20자이다() {
-        sku.setName("01234567890123456789");
-        assertTrue(sku.getName().length() <= 20);
+        sku = new Sku("s123", "01234567890123456789");
+        assertTrue(sku.getName().length() == 20);
 
-        sku.setName("012345678901234567890");
         assertThrows(IllegalArgumentException.class, () -> {
-            sku.isValidName(sku.getName());
+            sku = new Sku("s123", "012345678901234567890");
         });
     }
 
     @Test
     void 바코드를_등록할_수_있다() {
-        sku.addBarcode("00110011");
+        sku.addBarcode(Barcode.of("111", "01010000"));
         assertTrue(sku.getBarcode().size() == 1);
     }
 
     @Test
     void Sku의_초기상태는_런칭_예정이다() {
-        assertTrue(sku.getStatus().equals("런칭 예정"));
+        assertTrue(sku.getStatus().equals(SkuStatus.READY));
     }
 }
