f9fa3368aa287f8bc31936fe237c7f3e809d02d0
hyelin <sjgpfls@gmail.com>
2022-07-29 09:15:18 +0900

    🔨 Stock 수정

--- src/main/java/kvp/hhlin/minifullfillment/domain/Stock.java
+++ src/main/java/kvp/hhlin/minifullfillment/domain/Stock.java
@@ -8,17 +8,21 @@ public class Stock {
 
     private Long quantity;
 
-    Stock(String centerId, String skuCode) {
+    private Stock(String centerId, String skuCode) {
         this.centerId = centerId;
         this.skuCode = skuCode;
         this.quantity = 0L;
+    }
+
+    public static Stock of(String centerId, String skuCode) {
+        return new Stock(centerId, skuCode);
     }
 
     private boolean isValidQuantity() {
         return this.quantity >= 0L;
     }
 
-    private void isValidStatus() {
+    private void validateStatus() {
         if (!isValidQuantity()) {
             throw new IllegalArgumentException("재고는 0보다 작을 수 없습니다.");
         }
@@ -30,10 +34,7 @@ public class Stock {
 
     public void subtractQuantity(Long quantity) {
         this.quantity -= quantity;
-        isValidStatus();
+        validateStatus();
     }
 
-    public Long getQuantity() {
-        return quantity;
-    }
 }
